47caf77a90b01bd7925d848d9d84a3ee6c88983d
dokutan <54861821+dokutan@users.noreply.github.com>
2022-08-05 23:29:51 +0200

    Add project files and original source

---
+++ README.md
@@ -0,0 +1,10 @@
+# finite-distributions
+
+An updated version of clojure.contrib.probabilities.finite-distributions.
+
+## Usage
+
+FIXME
+
+## License
+EPL-1.0
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject finite-distributions "0.1.0-SNAPSHOT"
+  :description "An updated version of clojure.contrib.probabilities.finite-distributions"
+  :url "https://github.com/dokutan/finite-distributions"
+  :license {:name "EPL-1.0"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/algo.monads "0.1.6"]])
